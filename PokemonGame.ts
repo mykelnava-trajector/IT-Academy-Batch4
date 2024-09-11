782d1cd66f12672af103de69c96ed3c304bbf32c
unknown <Mykel.Nava@GTPH-LPC090.cscc.local>
2024-09-12 04:18:21 +0800

    Edited, kulang nalang ng laban along with interface

--- PokemonGame.ts
+++ PokemonGame.ts
@@ -5,37 +5,41 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const getYourPokemon = async() => {
+const getPokemon = async() => {
     rl.question('What is your pokemon? ', async(pokemonpick) => {
         const yourPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonpick}`)
-        const yourPokemondata= await yourPokemon.json();
-        console.log(`You picked: ${yourPokemondata.name}`)
+        const yourPokemondata = await yourPokemon.json();
+        console.log(`You picked:  ${yourPokemondata.name}`)
+        const yourPokemonsMove = await fetch(yourPokemondata.moves[0].move.url)
+        const yourPokemonMovedata = await yourPokemonsMove.json()
+        console.log(`The move of your pokemon is: ${yourPokemondata.moves[0].move.name}`)
+        console.log(`Accuracy: ${yourPokemonMovedata.accuracy}`)
+        console.log(`Your pokemon's health is: ${yourPokemondata.stats[0].base_stat}`)
+    rl.question('What is the enemy pokemon? ', async(enemypick) => {
+            const enemyPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${enemypick}`)
+            const enemyPokemondata= await enemyPokemon.json();
+            console.log(`Enemy picked: ${enemyPokemondata.name}`)
+            const enemyPokemonsMove = await fetch(enemyPokemondata.moves[0].move.url)
+            const enemyPokemonsMovedata = await enemyPokemonsMove.json()
+            console.log(`The move of your pokemon is: ${enemyPokemondata.moves[0].move.name}`)
+            console.log(`Accuracy: ${enemyPokemonsMovedata.accuracy}`)
+            console.log(`Your pokemon's health is: ${enemyPokemondata.stats[0].base_stat}`)
+        });
         
     });
     
  }  
- const getEnemyPokemon = async() => {
-    rl.question('What is the enemy pokemon? ', async(enemypick) => {
-        console.log(`Your pokemon is: ${enemypick}`)
-        const enemyPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${enemypick}`)
-        const enemyPokemondata= await enemyPokemon.json();
-        console.log(`You picked: ${enemyPokemondata.name}`)
-    });
-    
- }
  interface yourPokemonAbilities{
     hp:number;
     ability:string;
  }
-
-    
 
 console.log(`==============Pokemon Fight!==============`)
 rl.question('Do you want to continue? y/n\nAnswer: ', (menuanswer) => {
     let option = menuanswer
     switch(option){
         case 'y':
-         getYourPokemon()
+        getPokemon()
          break;
         case 'n':
          console.log('Goodbye. Press ctrl + c in order to fully exit.')
