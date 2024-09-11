6bca72c96b55067a18253619b4586cc485330af2
unknown <Mykel.Nava@GTPH-LPC090.cscc.local>
2024-09-12 04:27:31 +0800

    Edited the layout pero need pa yung laban saka interface

--- PokemonGame.ts
+++ PokemonGame.ts
@@ -12,17 +12,20 @@ const getPokemon = async() => {
         console.log(`You picked:  ${yourPokemondata.name}`)
         const yourPokemonsMove = await fetch(yourPokemondata.moves[0].move.url)
         const yourPokemonMovedata = await yourPokemonsMove.json()
+        console.log(`===================================`)
         console.log(`The move of your pokemon is: ${yourPokemondata.moves[0].move.name}`)
-        console.log(`Accuracy: ${yourPokemonMovedata.accuracy}`)
+        console.log(`Power of the move: ${yourPokemonMovedata.accuracy}`)
         console.log(`Your pokemon's health is: ${yourPokemondata.stats[0].base_stat}`)
+        console.log(`===================================`)
     rl.question('What is the enemy pokemon? ', async(enemypick) => {
             const enemyPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${enemypick}`)
             const enemyPokemondata= await enemyPokemon.json();
             console.log(`Enemy picked: ${enemyPokemondata.name}`)
             const enemyPokemonsMove = await fetch(enemyPokemondata.moves[0].move.url)
             const enemyPokemonsMovedata = await enemyPokemonsMove.json()
+            console.log(`===================================`)
             console.log(`The move of your pokemon is: ${enemyPokemondata.moves[0].move.name}`)
-            console.log(`Accuracy: ${enemyPokemonsMovedata.accuracy}`)
+            console.log(`Power of the move: ${enemyPokemonsMovedata.accuracy}`)
             console.log(`Your pokemon's health is: ${enemyPokemondata.stats[0].base_stat}`)
         });
         
